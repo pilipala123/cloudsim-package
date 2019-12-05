63f044a68ecec9c2acfeaccc776d00c4770e490f
xlx <1310438918@qq.com>
2019-12-05 17:13:35 +0800

    change3

--- modules/cloudsim/src/main/java/org/cloudbus/cloudsim/container/core/ServiceLoadBalancerGW.java
+++ modules/cloudsim/src/main/java/org/cloudbus/cloudsim/container/core/ServiceLoadBalancerGW.java
@@ -1,8 +1,9 @@
 package org.cloudbus.cloudsim.container.core;
 
-import org.cloudbus.cloudsim.Cloudlet;
-import org.cloudbus.cloudsim.container.core.SLB.CloudletMinParament;
-import org.cloudbus.cloudsim.container.core.SLB.SiemensContainerresource;
+import org.cloudbus.cloudsim.container.core.Siemens.BindContianer;
+import org.cloudbus.cloudsim.container.core.Siemens.CloudletMinParament;
+import org.cloudbus.cloudsim.container.core.Siemens.SiemensContainerresource;
+import org.cloudbus.cloudsim.container.core.Siemens.SiemensVmresources;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,7 +28,6 @@ public class ServiceLoadBalancerGW extends Container{
         /**
          * Functions to calculate response time and qps will be added here
          */
-        ;
         setResponseTime(processRequests(cloudletList,100,100));
         setQps(money);
         System.out.println("The GWSLB QPS is "+qps);
@@ -57,14 +57,17 @@ public class ServiceLoadBalancerGW extends Container{
     }
 
 
-    public List<SiemensContainerresource> createVmResource(int vmnumber, int cpuresources, int bwresources, int maxlength){
+    public List<SiemensContainerresource> createVmResource(int containernumber, int vmnumber, int cpuresources, int bwresources, int maxlength){
         List<SiemensContainerresource> siemensContainerresourceList =new ArrayList<> ();
-        for(Integer i=0;i<vmnumber;i++){
+        int containereveryvm = (int)containernumber/vmnumber;
+        for(Integer i=0;i<containernumber;i++){
             SiemensContainerresource siemensContainerresource = new SiemensContainerresource();
             siemensContainerresource.setId(i);
             siemensContainerresource.initCpuarraypool(cpuresources,maxlength);
             siemensContainerresource.initBwarraypool(bwresources,maxlength);
+            siemensContainerresource.setSiemensVmid((int)(i/containereveryvm));
             siemensContainerresourceList.add(siemensContainerresource);
+
         }
         return siemensContainerresourceList;
     }
@@ -74,30 +77,59 @@ public class ServiceLoadBalancerGW extends Container{
      * Process the requests and generate response time
      * @param cloudletList
      */
+    public List<BindContianer> bindCloudlet(List<ContainerCloudlet> cloudletList){
+        List<BindContianer> bindContianerList = new ArrayList<>();
+        int containerid = 0;
+
+        for (ContainerCloudlet cloudlet:cloudletList){
+            BindContianer bindContianer = new BindContianer(cloudlet.getCpurequest(),
+                    cloudlet.getBwrequest(),cloudlet.getCloudletId(),containerid,
+                    cloudlet.getStarttime(),(int)cloudlet.getCloudletLength());
+            bindContianerList.add(bindContianer);
+            cloudlet.setContainerId(bindContianer.getId());
+            containerid++;
+        }
+        return bindContianerList;
+    }
+
+    public List<SiemensVmresources> createsiemnesVmresources(int vmnumber){
+        List<SiemensVmresources> siemensVmresourcesList = new ArrayList<>();
+        for(int i=0;i<vmnumber;i++){
+            SiemensVmresources siemensVmresources = new SiemensVmresources();
+            siemensVmresources.setId(i);
+            siemensVmresourcesList.add(siemensVmresources);
+        }
+        return siemensVmresourcesList;
+
+    }
 
 
     public int processRequests(List<ContainerCloudlet> cloudletList,int cpuresources,int bwresources){
         int response_time=0;
-        int vmnumber=3;
-        int cloudlethandletime;
+        int containernumber=18;
+        int vmnumber = 6;
+        int containerhandletime;
         int time = 0;
-        int cloudletcpurequest=0,remaincpuresources=0;
-        int cloudletbwrequest=0,remainbwresources=0;
-        int vmfreenumber = 0;
-        int cpuusage = 0,bwusage = 0;
+        int containercpurequest=0,remaincpuresources=0;
+        int containerbwrequest=0,remainbwresources=0;
+        int vmfreenumber = 0,vmcpuusage=0,vmbwusage=0;
+        int containercpuusage = 0,containerbwusage = 0;
         int finishcloudletnumber=0;
         CloudletMinParament cloudletMinParament = new CloudletMinParament();
         cloudletMinParament.setcloudletMinParament(cloudletList);
-        List<SiemensContainerresource> siemensContainerresourceList = createVmResource(vmnumber,cpuresources,bwresources,cloudletMinParament.getMaxtimelength());
+        List<SiemensVmresources> siemensVmresourcesList = createsiemnesVmresources(vmnumber);
+        List<SiemensContainerresource> siemensContainerresourceList = createVmResource(containernumber,vmnumber,cpuresources,bwresources,cloudletMinParament.getMaxtimelength());
+
+        List<BindContianer> bindCloudletlist =bindCloudlet(cloudletList);
 
         while (true) {
 
             vmfreenumber = 0;
-            for (ContainerCloudlet cloudlet : cloudletList) {
-                if (cloudlet.getState() == 3) {
+            for (BindContianer bindContianer : bindCloudletlist) {
+                if (bindContianer.getState() == 3) {
                     continue;
                 }
-                if (cloudlet.getStarttime()>time){
+                if (bindContianer.getStarttime()>time){
                     break;
                 }
                 for (SiemensContainerresource siemensContainerresource : siemensContainerresourceList) {
@@ -106,65 +138,82 @@ public class ServiceLoadBalancerGW extends Container{
                     remaincpuresources = siemensContainerresource.getRemaincpuresource();
                     remainbwresources = siemensContainerresource.getRemainbwresource();
 
-                    if (cloudlet.getState() == 0 && cloudlet.getStarttime() == time) {
-                        cloudlet.setState(1);
+                    if (bindContianer.getState() == 0 && bindContianer.getStarttime() == time) {
+                        bindContianer.setState(1);
                     }
-                    if (cloudlet.getState() == 1 || cloudlet.getState() == 2) {
+                    if (bindContianer.getState() == 1 || bindContianer.getState() == 2) {
 
-                        cloudletcpurequest = cloudlet.getCpurequest();
-                        cloudletbwrequest = cloudlet.getBwrequest();
-                        if ((remaincpuresources >= cloudletcpurequest)
-                                && (remainbwresources >= cloudletbwrequest)) {
-                            cloudlethandletime = (int) cloudlet.getCloudletLength();
-                            siemensContainerresource.changeCpuarraypool(remaincpuresources, cloudletcpurequest, time, cloudlethandletime);
-                            siemensContainerresource.changeBwarraypool(remainbwresources, cloudletbwrequest, time, cloudlethandletime);
-                            cloudlet.setState(3);
-                            cloudlet.setFinishtime(time);
+                        containercpurequest = bindContianer.getCpuusage();
+                        containerbwrequest = bindContianer.getBwusage();
+                        if ((remaincpuresources >= containercpurequest)
+                                && (remainbwresources >= containerbwrequest)) {
+                            containerhandletime = (int) bindContianer.getHandletime();
+                            siemensContainerresource.changeCpuarraypool(remaincpuresources, containercpurequest, time, containerhandletime);
+                            siemensContainerresource.changeBwarraypool(remainbwresources, containerbwrequest, time, containerhandletime);
+                            bindContianer.setState(3);
+                            bindContianer.setFinishtime(time);
                             finishcloudletnumber++;
                             break;
                         }
 
                         if ((remaincpuresources < cloudletMinParament.getMincpurequest()
                                 || remainbwresources < cloudletMinParament.getMinbwrequest())
-                                && (siemensContainerresource.getId()==vmnumber-1)) {
-                            cloudlet.setState(2);
+                                && (siemensContainerresource.getId()==containernumber-1)) {
+                            bindContianer.setState(2);
                             break;
                         }
                     }
 
                 }
             }
-            for(SiemensContainerresource siemensContainerresource : siemensContainerresourceList){
-                cpuusage = 0;
-                bwusage = 0;
-                for (int i = 0; i < cpuresources; i++) {
-                    if (siemensContainerresource.getCpuarraypool()[i][time] == 1) {
-                        cpuusage++;
+            for(SiemensVmresources siemensVmresources : siemensVmresourcesList) {
+                siemensVmresources.setCpuusage(0);
+                siemensVmresources.setBwusage(0);
+                for (SiemensContainerresource siemensContainerresource : siemensContainerresourceList) {
+                    containercpuusage = 0;
+                    containerbwusage = 0;
+                    if(siemensContainerresource.getSiemensVmid()!= siemensVmresources.getId()){
+                        continue;
                     }
-                    if (siemensContainerresource.getBwarraypool()[i][time] == 1) {
-                        bwusage++;
+                    for (int i = 0; i < cpuresources; i++) {
+                        if (siemensContainerresource.getCpuarraypool()[i][time] == 1) {
+                            containercpuusage++;
+                        }
+                        if (siemensContainerresource.getBwarraypool()[i][time] == 1) {
+                            containerbwusage++;
+                        }
                     }
+
+                    siemensVmresources.addCpuusage(containercpuusage);
+                    siemensVmresources.addBwusage(containerbwusage);
+
+
+                    System.out.println("At time:" + time + "ms Container:" + siemensContainerresource.getId() + " Cpu usage is " + containercpuusage);
+                    System.out.println("At time:" + time + "ms Container:" + siemensContainerresource.getId() + " Bw usage is " + containerbwusage);
+
                 }
-                if (cpuusage==0&&bwusage==0){
+                vmcpuusage = siemensVmresources.getCpuusage();
+                vmbwusage = siemensVmresources.getBwusage();
+                if (vmcpuusage == 0 && vmbwusage == 0) {
                     vmfreenumber++;
                 }
-                System.out.println("At time:"+ time +"ms VM:"+ siemensContainerresource.getId()+" Cpu usage is "+cpuusage);
-                System.out.println("At time:"+ time +"ms VM:"+ siemensContainerresource.getId()+ " Bw usage is "+bwusage);
+                System.out.println("At time:" + time + "ms VM:" + siemensVmresources.getId() + " Cpu usage is " + vmcpuusage);
+                System.out.println("At time:" + time + "ms VM:" + siemensVmresources.getId() + " Bw usage is " + vmbwusage);
+
             }
-            if(vmfreenumber ==vmnumber){
+            if (vmfreenumber == vmnumber) {
                 break;
             }
-
             time++;
 
         }
-        for(Cloudlet cloudlet:cloudletList){
-            int cloudletstarttime = cloudlet.getStarttime();
-            int cloudletfinishtime = cloudlet.getFinishtime();
+        for(BindContianer bindContianer : bindCloudletlist){
+            int cloudletstarttime = bindContianer.getStarttime();
+            int cloudletfinishtime = bindContianer.getFinishtime();
         }
 
         System.out.println("finish cloudlet numbers is "+finishcloudletnumber);
-        System.out.println("The response time of SLB is "+time+"ms");
+        System.out.println("The response time of Siemens is "+time+"ms");
 //        System.out.println("The Response Time of GWslb is "+ response_time+"ms");
         return time;
     }
